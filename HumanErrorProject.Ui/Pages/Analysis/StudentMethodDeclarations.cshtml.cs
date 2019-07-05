3a098bf4984015e1b977e02380a78bd9f1591160
CobeGreene <cobegreene@gmail.com>
2019-07-04 21:25:02 -0700

    Implement the analysis pages for the method declarations.

---
+++ HumanErrorProject.Ui/Pages/Analysis/StudentMethodDeclarations.cshtml.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HumanErrorProject.Data.DataAccess;
+using HumanErrorProject.Data.Models;
+using HumanErrorProject.Ui.Constants;
+using HumanErrorProject.Ui.DataVisuals;
+using HumanErrorProject.Ui.Options;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace HumanErrorProject.Ui.Pages.Analysis
+{
+    [Authorize(Roles = IdentityRoleConstants.Admin)]
+    public class StudentMethodDeclarationsModel : PageModel
+    {
+        public HumanErrorProjectContext Context;
+        public DbSet<Assignment> Assignments;
+
+        public StudentMethodDeclarationsModel(HumanErrorProjectContext context)
+        {
+            Context = context;
+            Assignments = context.Set<Assignment>();
+        }
+
+        [FromRoute]
+        public int Id { get; set; }
+
+        [FromRoute]
+        public int StudentId { get; set; }
+
+        public Assignment Assignment { get; set; }
+
+        public Student Student { get; set; }
+
+        public IList<Snapshot> Snapshots { get; set; } = new List<Snapshot>();
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Assignment = await Assignments.FindAsync(Id);
+            if (Assignment == null) return NotFound();
+
+            Snapshots = Assignment.Snapshots.Where(x => x.StudentId.Equals(StudentId)).
+                Select(x =>
+                {
+                    Context.Entry(x).Reference(y => y.Student).Load();
+                    Context.Entry(x).Reference(y => y.Report).Load();
+                    if (x.Report.Type == SnapshotReport.SnapshotReportTypes.Success)
+                    {
+                        Context.Entry((SnapshotSuccessReport)x.Report).Collection(y => y.SnapshotMethods).Query()
+                            .Include(y => y.MethodDeclaration).Load();
+                    }
+                    return x;
+                }).ToList();
+
+            if (Snapshots.Count == 0) return NotFound();
+
+            Student = Snapshots.First().Student;
+
+            Context.Entry(Assignment).Reference(x => x.Solution)
+                .Query().Include(x => x.MethodDeclarations).Load();
+
+            return Page();
+        }
+
+        public Task<IEnumerable<Snapshot>> GetBuildSnapshots()
+        {
+            return Task.FromResult(Snapshots.Where(x => x.Report.Type == SnapshotReport.SnapshotReportTypes.Success));
+        }
+
+
+        public BarChart GetOverallMethodDeclarationsChart(IList<Snapshot> snapshots)
+        {
+            return new BarChart()
+            {
+                Id = "overall_method_declaration",
+                Minimum = 0,
+                Colors = new List<string>()
+                {
+                    DataVisualConstants.FailedColor,
+                    DataVisualConstants.PassedColor,
+                    DataVisualConstants.PrimaryColor
+                },
+                Labels = new List<string>()
+                {
+                    "Worst",
+                    "Best",
+                    "Average"
+                },
+                Values = new List<int>()
+                {
+                    snapshots.Select(x => ((SnapshotSuccessReport)x.Report).SnapshotMethods.Count(y => y.Declared)).Min(),
+                    snapshots.Select(x => ((SnapshotSuccessReport)x.Report).SnapshotMethods.Count(y => y.Declared)).Max(),
+                    (int)snapshots.Select(x => ((SnapshotSuccessReport)x.Report).SnapshotMethods.Count(y => y.Declared)).Average(),
+                }
+            };
+        }
+
+    }
+}
---
+++ HumanErrorProject.Ui/Pages/Components/AssignmentMethodDeclarationsProgress/AssignmentMethodDeclarationsProgressViewComponent.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HumanErrorProject.Data.Models;
+using HumanErrorProject.Ui.Constants;
+using HumanErrorProject.Ui.DataVisuals;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HumanErrorProject.Ui.Pages.Components.AssignmentMethodDeclarationsProgress
+{
+    public class AssignmentMethodDeclarationsProgressViewComponent : ViewComponent
+    {
+
+        public IViewComponentResult Invoke(IList<Snapshot> snapshots)
+        {
+            return View("Default", GetLineChart(snapshots));
+        }
+
+        public LineChart GetLineChart(IList<Snapshot> snapshots)
+        {
+            var tests = snapshots.OrderBy(x => x.SnapshotSubmission.CreatedDateTime)
+                .Select(NumberOfDeclaredOrDefault).ToList();
+
+            return new LineChart()
+            {
+                Id = "method_declarations_progress",
+                YValues = tests,
+                XValues = tests.Select((x, iter) => iter + 1).ToList(),
+                Colors = tests.Select(GetColorOrDefault).ToList(),
+            };
+        }
+
+        public int NumberOfDeclaredOrDefault(Snapshot snapshot)
+        {
+            if (snapshot.Report.Type == Data.Models.SnapshotReport.SnapshotReportTypes.Failure)
+                return 0;
+            var report = (SnapshotSuccessReport)snapshot.Report;
+            return report.SnapshotMethods.Count(x => x.Declared);
+        }
+
+        public string GetColorOrDefault(int number)
+        {
+            return number == 0 ? DataVisualConstants.FailedColor : DataVisualConstants.PassedColor;
+        }
+    }
+}
