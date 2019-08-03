52b4fd2766f9cd6b0f2b5073e5a3421ff62317b3
CobeGreene <cobegreene@gmail.com>
2019-08-02 18:06:53 -0700

    Graph with nodes chart.

---
+++ HumanErrorProject.Ui/DataVisuals/GraphChart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HumanErrorProject.Ui.DataVisuals
+{
+    public class GraphChart
+    {
+        public string Id { get; set; }
+        public IList<GraphChartNode> GraphChartNodes { get; set; } = new List<GraphChartNode>();
+        public IList<GraphCartLink> GraphCartLinks { get; set; } = new List<GraphCartLink>();  
+    }
+}
--- HumanErrorProject.Ui/Pages/Analysis/Markov/Details.cshtml.cs
+++ HumanErrorProject.Ui/Pages/Analysis/Markov/Details.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using HumanErrorProject.Data.DataAccess;
 using HumanErrorProject.Data.Models;
 using HumanErrorProject.Ui.Constants;
+using HumanErrorProject.Ui.DataVisuals;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -29,6 +30,8 @@ namespace HumanErrorProject.Ui.Pages.Analysis.Markov
 
         public MarkovModel MarkovModel { get; set; }
 
+        public GraphChart GraphChart { get; set; }
+
         public async Task<IActionResult> OnGetAsync()
         {
             MarkovModel = await MarkovModels.FindAsync(Id);
@@ -43,7 +46,46 @@ namespace HumanErrorProject.Ui.Pages.Analysis.Markov
                 .Query().Include(x => x.Snapshots)
                 .Include(x => x.Transitions).Load();
 
+            GraphChart = GetGraphChart();
+
             return Page();
         }
+
+        public GraphChart GetGraphChart()
+        {
+            if (!MarkovModel.Finished) return null;
+
+            return new GraphChart()
+            {
+                Id = "overall_graph",
+                GraphChartNodes = new List<GraphChartNode>()
+                {
+                    new GraphChartNode() { Id = 1, Name = "A", Color = DataVisualConstants.PassedColor },
+                    new GraphChartNode() { Id = 2, Name = "B", Color = DataVisualConstants.PassedColor },
+                    new GraphChartNode() { Id = 3, Name = "C", Color = DataVisualConstants.PassedColor },
+                    new GraphChartNode() { Id = 4, Name = "D", Color = DataVisualConstants.PassedColor },
+                    new GraphChartNode() { Id = 5, Name = "E", Color = DataVisualConstants.PassedColor },
+                    new GraphChartNode() { Id = 6, Name = "F", Color = DataVisualConstants.PassedColor },
+                    new GraphChartNode() { Id = 7, Name = "G", Color = DataVisualConstants.PassedColor },
+                    new GraphChartNode() { Id = 8, Name = "I", Color = DataVisualConstants.PassedColor },
+                    new GraphChartNode() { Id = 9, Name = "J", Color = DataVisualConstants.PassedColor },
+                },
+                GraphCartLinks = new List<GraphCartLink>()
+                {
+                    new GraphCartLink() { Source = 1, Target = 2, Color = DataVisualConstants.PrimaryColor },
+                    new GraphCartLink() { Source = 1, Target = 5, Color = DataVisualConstants.PrimaryColor },
+                    new GraphCartLink() { Source = 1, Target = 6, Color = DataVisualConstants.PrimaryColor },
+                    new GraphCartLink() { Source = 2, Target = 3, Color = DataVisualConstants.PrimaryColor },
+                    new GraphCartLink() { Source = 2, Target = 7, Color = DataVisualConstants.PrimaryColor },
+                    new GraphCartLink() { Source = 3, Target = 4, Color = DataVisualConstants.PrimaryColor },
+                    new GraphCartLink() { Source = 8, Target = 3, Color = DataVisualConstants.PrimaryColor },
+                    new GraphCartLink() { Source = 4, Target = 5, Color = DataVisualConstants.PrimaryColor },
+                    new GraphCartLink() { Source = 5, Target = 9, Color = DataVisualConstants.PrimaryColor },
+                }
+            };
+
+            throw new NotImplementedException();
+        }
+
     }
 }
