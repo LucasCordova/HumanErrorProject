8bb36f0c231926ad8f96f68a86a05f7eb8cf86bc
CobeGreene <cobegreene@gmail.com>
2019-08-06 14:12:51 -0700

    Add state features.

--- HumanErrorProject.Ui/Pages/Analysis/Markov/State.cshtml.cs
+++ HumanErrorProject.Ui/Pages/Analysis/Markov/State.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using HumanErrorProject.Data.DataAccess;
 using HumanErrorProject.Data.Models;
 using HumanErrorProject.Ui.Constants;
+using HumanErrorProject.Ui.DataVisuals;
 using HumanErrorProject.Ui.Utilities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -19,6 +20,7 @@ namespace HumanErrorProject.Ui.Pages.Analysis.Markov
         public HumanErrorProjectContext Context;
         public DbSet<MarkovModel> MarkovModels;
         public DbSet<Snapshot> Snapshots;
+        public DbSet<SurveyQuestion> SurveyQuestions;
         public ColorHelper ColorHelper;
 
         public StateModel(HumanErrorProjectContext context, ColorHelper colorHelper)
@@ -26,6 +28,7 @@ namespace HumanErrorProject.Ui.Pages.Analysis.Markov
             Context = context;
             MarkovModels = context.Set<MarkovModel>();
             Snapshots = context.Set<Snapshot>();
+            SurveyQuestions = context.Set<SurveyQuestion>();
             ColorHelper = colorHelper;
         }
 
@@ -40,6 +43,13 @@ namespace HumanErrorProject.Ui.Pages.Analysis.Markov
 
         public string ReturnUrl { get; set; }
 
+        public IList<Snapshot> MarkovSnapshots { get; set; } = new List<Snapshot>();
+        public int DeletedSnapshots { get; set; }
+
+        public IList<Snapshot> SuccessfulSnapshots { get; set; } = new List<Snapshot>(); 
+
+        public IList<Survey> Surveys { get; set; } = new List<Survey>();
+
         public async Task<IActionResult> OnGetAsync()
         {
             MarkovModel = await MarkovModels.FindAsync(Id);
@@ -48,6 +58,7 @@ namespace HumanErrorProject.Ui.Pages.Analysis.Markov
 
             Context.Entry(MarkovModel).Reference(x => x.Assignment)
                 .Query().Include(x => x.CourseClass)
+                .Include(x => x.TestProject).ThenInclude(x => x.UnitTests)
                 .Load();
 
             Context.Entry(MarkovModel).Collection(x => x.States)
@@ -64,10 +75,100 @@ namespace HumanErrorProject.Ui.Pages.Analysis.Markov
                 State,
             });
 
+            MarkovModelState.Snapshots.ToList()
+                .ForEach(x => MarkovSnapshots
+                    .Add(Snapshots
+                        .Include(y => y.SnapshotSubmission)
+                        .Include(y => y.Student)
+                        .Include(y => y.Survey)
+                        .Include(y => y.Report)
+                        .FirstOrDefault(y => y.Id.Equals(x.SnapshotId))));
 
+            DeletedSnapshots =
+                MarkovSnapshots.Count(x => 
+                    MarkovModelState
+                        .Snapshots.All(y => !y.SnapshotId.Equals(x.Id)));
+
+            Surveys = MarkovSnapshots.Select(x =>
+            {
+                Context.Entry(x.Survey).Collection(y => y.SurveyResponses).Query()
+                    .Include(y => y.Answer).Include(y => y.Question).Load();
+                return x.Survey;
+            }).ToList();
+
+            SuccessfulSnapshots = MarkovSnapshots.Where(x => x.Report.Type.Equals(SnapshotReport.SnapshotReportTypes.Success))
+                .Select(x =>
+                {
+                    Context.Entry((SnapshotSuccessReport)x.Report).Collection(y => y.UnitTestResults).Query()
+                        .Include(y => y.UnitTest).Load();
+                    return x;
+                }).ToList();
 
             return Page();
         }
+
+        public IList<SurveyAnswer> GetSurveyAnswers(SurveyQuestion question)
+        {
+            return Surveys.Where(x => x.IsCompleted)
+                .SelectMany(x => x.SurveyResponses)
+                .Where(x => x.SurveyQuestionId.Equals(question.Id))
+                .Select(x => x.Answer).ToList();
+        }
+
+        public DonutChart GetOverallSurveyChart()
+        {
+            var complete = Surveys.Count(x => x.IsCompleted);
+            var incomplete = Surveys.Count(x => !x.IsCompleted);
+            return new DonutChart()
+            {
+                Id = "overall_survey_chart",
+                Colors = new List<string>()
+                {
+                    DataVisualConstants.PassedColor,
+                    DataVisualConstants.FailedColor,
+                },
+                Values = new List<int>()
+                {
+                    complete,
+                    incomplete
+                },
+                Text = $"{Math.Floor((double)complete / (complete + incomplete) * 100):F0}%"
+            };
+        }
+
+        public BoxAndWhiskerChart GetBoxAndWhiskerChart(SurveyQuestionRate question, IList<SurveyAnswerRate> answers)
+        {
+            return new BoxAndWhiskerChart()
+            {
+                Color = DataVisualConstants.PassedColor,
+                Id = $"question_{question.Id}",
+                Values = answers.Select(x => x.Selection).ToList()
+            };
+        }
+
+        public DonutChart GetOverallBuildChart()
+        {
+            var success = MarkovSnapshots.Count(x =>
+                x.Report.Type == Data.Models.SnapshotReport.SnapshotReportTypes.Success);
+            var failure = MarkovSnapshots.Count(x =>
+                x.Report.Type == Data.Models.SnapshotReport.SnapshotReportTypes.Failure);
+            return new DonutChart()
+            {
+                Id = "assignment_overall_builds",
+                Colors = new List<string>()
+                {
+                    Constants.DataVisualConstants.PassedColor,
+                    Constants.DataVisualConstants.FailedColor
+                },
+                Values = new List<int>()
+                {
+                    success,
+                    failure
+                },
+                Text = $"{Math.Floor((double)success / (success + failure) * 100):F0}%"
+            };
+        }
+
 
     }
 }
